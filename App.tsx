0856e3e7d0d9d1f02d4870d895564e203df2234f
arhamali06 <arhamali111333@gmail.com>
2026-08-22 20:35:40 +0500

    minor changes

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 
 import SplashScreen from './src/screens/SplashScreen';
 import LoginScreen from './src/screens/LoginScreen';
-
 import HomeScreen from './src/screens/HomeScreen';
 import ExploreScreen from './src/screens/ExploreScreen';
 import CartScreen from './src/screens/CartScreen';
